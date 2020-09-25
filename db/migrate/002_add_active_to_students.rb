6b094ab0ae32581ab00a031da4c5909f5aa3359d
Austin Cummings <aac729@live.com>
2020-09-25 17:39:38 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_active_to_students.rb
+++ db/migrate/002_add_active_to_students.rb
@@ -1,7 +1,7 @@
 class AddActiveToStudents < ActiveRecord::Migration 
   
   def change 
-    add_column :students, :active, :boolean, :default => 
+    add_column :students, :active, :boolean, :default => false  
   end 
   
 end 
